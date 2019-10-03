4d4de3be8246e5e99b694371e8e2e07411e7f66c
Lopè Ariyo <lopeariyo@gmail.com>
2019-10-03 06:54:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var kittens = ["Milo","Otis","Garfield"] //define your array here
 // Add your functions and code here
 var destructivelyAppendKitten = kittens
 
-//var destructivelyPrependKitten = 
+//var destructivelyPrependKitten =
 //var destructivelyRemoveLastKitten
 //var destructivelyRemoveFirstKitten
 //var
